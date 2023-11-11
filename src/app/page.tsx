7f0502a0f06bc0f3b7d032240ca8e38200c4003e
Aristos <92493374+axanthus@users.noreply.github.com>
2023-11-11 15:02:03 -0800

    add lib folders for api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
 
   //add your api calls here using Fetch
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/1`, {
+    next: { revalidate: 10 }, //every 10 seconds, revalidate data (ISR)
+  }) 
+  const data = await response.json()
+  console.log(data)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>{JSON.stringify(data) ?? null}</div>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
