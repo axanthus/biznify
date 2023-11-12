1a21ed249bb4d1fcfa432e688b4461b7de25cd28
nkenny160 <nkenny@scu.edu>
2023-11-12 07:03:44 -0800

    api requests won't work grrrr

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+// Import the 'useEffect' and 'useState' hooks from React
 import React, { useState, useEffect } from 'react';
 import Map from '../components/map';
 import SearchBar from '../components/SearchBar';
@@ -8,19 +9,27 @@ const YourPage: React.FC = () => {
     const [locations, setLocations] = useState([]);
 
     useEffect(() => {
-        // Fetch JSON data
-        fetch('/sampleLocations.json') // Replace with the correct path
-            .then(response => response.json())
-            .then(data => setLocations(data))
-            .catch(error => console.error('Error fetching data:', error));
-    }, []);
-    //const [locations, setLocations] = useState(initialLocations);
+        // Fetch data from the 'parseData' API
+        const fetchData = async () => {
+            try {
+                const response = await fetch('/api/parseData');
+                const data = await response.json();
+                setLocations(data.parsedData);
+                console.log(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []); // The empty dependency array ensures that this effect runs only once, similar to componentDidMount in class components
+
     const [selectedLocation, setSelectedLocation] = useState(locations[0]?.coords);
-    const [mapZoom, setMapZoom] = useState(13); //Default zoom
+    const [mapZoom, setMapZoom] = useState(13);
 
     const handleLocationClick = (coords: { lat: number; lng: number }) => {
-    setSelectedLocation(coords);
-    setMapZoom(15); //zoom in on specific location when clicked
+        setSelectedLocation(coords);
+        setMapZoom(15);
     };
 
     const handleSearchWrapper = async (address: string) => {
@@ -28,22 +37,22 @@ const YourPage: React.FC = () => {
     };
 
     return (
-    <div style={{ display: 'flex' }}>
-        <div style={{ flex: 1, padding: '20px' }}>
-            <h1>Your Page Title</h1>
-            <SearchBar onSearch={handleSearchWrapper} />
-            <ul>
-                {locations.map((location, index) => (
-                <li key={index} onClick={() => handleLocationClick(location.coords)}>
-                    {location.title}
-                </li>
-                ))}
-            </ul>
+        <div style={{ display: 'flex' }}>
+            <div style={{ flex: 1, padding: '20px' }}>
+                <h1>Your Page Title</h1>
+                <SearchBar onSearch={handleSearchWrapper} />
+                <ul>
+                    {locations.map((location, index) => (
+                        <li key={index} onClick={() => handleLocationClick(location.coords)}>
+                            {location.title}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div style={{ flex: 2, position: 'relative' }}>
+                <Map locations={locations.map(location => location.coords)} center={selectedLocation} zoom={mapZoom} />
+            </div>
         </div>
-        <div style={{ flex: 2, position: 'relative' }}>
-        <Map locations={locations.map(location => location.coords)} center={selectedLocation} zoom={mapZoom} />
-        </div>
-    </div>
     );
 };
 
