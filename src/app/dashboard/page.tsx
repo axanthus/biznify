394b95a33f7f4b7f8f05565a3cf286c534596154
nkenny160 <nkenny@scu.edu>
2023-11-11 19:06:03 -0800

    can import locations as JSON now

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,57 +1,45 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Map from '../components/map';
 
 const YourPage: React.FC = () => {
-  const initialLocations = [
-    {
-      title: "75 Spear St",
-      address1: "75 Spear St",
-      address2: "San Francisco, CA 94105, USA",
-      coords: { lat: 37.79273456511728, lng: -122.3940051607132 },
-      placeId: "ChIJV6BUEWSAhYAR5Wy7l6jxfN4",
-    },
-    {
-      title: "44 2nd St",
-      address1: "44 2nd St",
-      address2: "San Francisco, CA 94105, USA",
-      coords: { lat: 37.788599172528805, lng: -122.40071294536895 },
-      placeId: "ChIJt8SJmGKAhYARrLpvxawQyqs",
-    },
-    {
-      title: "2060 Fillmore St",
-      address1: "2060 Fillmore St",
-      address2: "San Francisco, CA 94115, USA",
-      coords: { lat: 37.78885222981391, lng: -122.4336785509262 },
-      placeId: "ChIJqfjKtseAhYARcij-Ox2I3os",
-    },
-  ];
-  const [locations, setLocations] = useState(initialLocations);
-  const [selectedLocation, setSelectedLocation] = useState(locations[0]?.coords);
-  const [mapZoom, setMapZoom] = useState(13); //Default zoom
+    const [locations, setLocations] = useState([]);
 
-  const handleLocationClick = (coords: { lat: number; lng: number }) => {
+    useEffect(() => {
+        // Fetch JSON data
+        fetch('/sampleLocations.json') // Replace with the correct path
+            .then(response => response.json())
+            .then(data => setLocations(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+  
+  
+    //const [locations, setLocations] = useState(initialLocations);
+    const [selectedLocation, setSelectedLocation] = useState(locations[0]?.coords);
+    const [mapZoom, setMapZoom] = useState(13); //Default zoom
+
+    const handleLocationClick = (coords: { lat: number; lng: number }) => {
     setSelectedLocation(coords);
     setMapZoom(15); //zoom in on specific location when clicked
-  };
+    };
 
-  return (
+    return (
     <div style={{ display: 'flex' }}>
-      <div style={{ flex: 1, padding: '20px' }}>
+        <div style={{ flex: 1, padding: '20px' }}>
         <h1>Your Page Title</h1>
         <ul>
-          {locations.map((location, index) => (
+            {locations.map((location, index) => (
             <li key={index} onClick={() => handleLocationClick(location.coords)}>
-              {location.title}
+                {location.title}
             </li>
-          ))}
+            ))}
         </ul>
-      </div>
-      <div style={{ flex: 2, position: 'relative' }}>
+        </div>
+        <div style={{ flex: 2, position: 'relative' }}>
         <Map locations={locations.map(location => location.coords)} center={selectedLocation} zoom={mapZoom} />
-      </div>
+        </div>
     </div>
-  );
+    );
 };
 
 export default YourPage;
